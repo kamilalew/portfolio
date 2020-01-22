39fffcbcfddf4a5250b8ba8d58d81cf15e1fe002
Kamila <kamila.m.lewicka@gmail.com>
2020-01-22 09:02:54 +0000

    Portfolio_app_created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div>This will be my portfolio !</div>
+    </div>
+  );
+}
+
+export default App;
