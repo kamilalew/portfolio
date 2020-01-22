088a6a1e1b358fc6aecc6f37ad8de2ff599d48d6
Kamila <kamila.m.lewicka@gmail.com>
2020-01-22 15:43:05 +0000

    Technologies added in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import reactIcon from './images/react.svg';
+import cssIcon from './images/css.svg';
+import html5Icon from './images/html5.svg';
+import jsIcon from './images/js.png';
 
 function App() {
   return (
@@ -23,6 +27,25 @@ function App() {
           </div>
           <div className='about_me_photo'></div>
         </div>
+        <div className='technologies_container'>
+          <div className='technologies_box'>
+            <div className='forFlex'>
+              <img className='reactIcon' src={reactIcon} alt='react icon' />
+              </div>
+              <div className='forFlex'>
+              <img className='reactIcon' src={cssIcon} alt='react icon' />
+              </div>
+              <div className='forFlex'>
+              <img className='reactIcon' src={html5Icon} alt='react icon' />
+              </div>
+              <div className='forFlex'>
+              <img className='reactIcon' src={jsIcon} alt='react icon' /> 
+              </div> 
+        </div>
+        <div className='technologies_known'>These are the technologies that I've learnt through completing various courses. 
+        I've used self-teaching sources like freecodecamp, W3schools and in-house training via Kodiri bootcamp</div>
+        </div>
+        <div className='projects'>My projects</div>
       </div>
     </div>
   );
