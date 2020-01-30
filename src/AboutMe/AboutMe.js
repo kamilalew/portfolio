d19503dc818d429e315204f1c769ee3dd795a887
Kamila <kamila.m.lewicka@gmail.com>
2020-01-30 17:11:00 +0000

    AboutMe component - further changes to desktop view

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './AboutMe.css';
 
 
-export default function ABoutMe() {
+export default function AboutMe() {
     return (
         <div className='AboutMe_container'>
           <div className='about_me_description'>Junior Front End Web developer looking for my first role.
+          For many years I've managed multiple international research projects. Now it's time to code !
+          Junior Front End Web developer looking for my first role.
           For many years I've managed multiple international research projects. Now it's time to code !
           </div>
           <div className='about_me_photo'></div>
