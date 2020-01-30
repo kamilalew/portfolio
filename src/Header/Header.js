0ab7c565c2cb8cec7108930a0319d1d4999e10c4
Kamila <kamila.m.lewicka@gmail.com>
2020-01-30 15:10:22 +0000

    Added logos to header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Header.css'
+import github_logo from '../images/github.svg';
+import linkedin_logo from '../images/linkedin.svg';
+import gmail_logo from '../images/gmail.svg';
 
 export default function Header() {
         return (
@@ -7,9 +10,9 @@ export default function Header() {
                 <div className='Header-name'>Kamila Lewicka - Junior Front End Developer
                 </div>
                 <ul className='header_information'>
-                    <li>Github</li>
-                    <li>Linkedin</li>
-                    <li>One More</li>
+                    <li><img className='Technology_logo' src={github_logo} alt='Github logo'/></li>
+                    <li><img className='Technology_logo' src={linkedin_logo} alt='LinkedIn logo'/></li>
+                    <li><img className='Technology_logo' src={gmail_logo} alt='Gmail logo'/></li>
                 </ul>
             </div >
         )
