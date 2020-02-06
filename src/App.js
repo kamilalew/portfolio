7795146fbb8feaa4542453abc899436a1b6beaa5
Kamila <kamila.m.lewicka@gmail.com>
2020-02-06 20:04:08 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import './App.css';
-import reactIcon from './images/react.svg';
-import cssIcon from './images/css.svg';
-import html5Icon from './images/html5.svg';
-import jsIcon from './images/js.png';
-import Header from './Header/Header.js';
-import Welcome from './Welcome/Welcome.js';
-import AboutMe from './AboutMe/AboutMe.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Welcome />
-      <AboutMe />
-        <div className='technologies_container'>
-          <div className='technologies_box'>
-            <div className='forFlex'>
-              <img className='reactIcon' src={reactIcon} alt='react icon' />
-            </div>
-            <div className='forFlex'>
-              <img className='reactIcon' src={cssIcon} alt='react icon' />
-            </div>
-            <div className='forFlex'>
-              <img className='reactIcon' src={html5Icon} alt='react icon' />
-            </div>
-            <div className='forFlex'>
-              <img className='reactIcon' src={jsIcon} alt='react icon' />
-            </div>
-          </div>
-          <div className='technologies_known'>These are the technologies that I've learnt through completing various courses.
-        I've used self-teaching sources like freecodecamp, W3schools and in-house training via Kodiri bootcamp</div>
+    <div className="container">
+      <div className="box a">
+        <div className="initials">K <span>L</span>
         </div>
-        <div className='projects'>My projects</div>
       </div>
+      <div className="box z">
+        <div className="aboutMe">JUNIOR FRONT END WEB DEVELOPER LOOKING FOR MY FIRST ROLE.
+      FOR MANY YEARS I'VE MANAGED MULTIPLE INTERNATIONAL RESEARCH PROJECTS. NOW IT'S TIME TO CODE !</div>
+      </div>
+      <div className="box c">3</div>
+      <div className="box d">4</div>
+      <div className="box e">5</div>
+      <div className="box f">6</div>
+      <div className="box g">7</div>
+      <div className="box h">8</div>
+      <div className="box i">9</div>
+      <div className="box j">10</div>
+      <div className="box k">10</div>
+      <div className="box l">10</div>
+      <div className="box m">10</div>
+      <div className="box n">10</div>
+      <div className="box o">10</div>
+      <div className="box p">10</div>
+      <div className="box q">10</div>
+      <div className="box r">10</div>
+      <div className="box s">10</div>
+      <div className="box t">10</div>
+      <div className="box u">10</div>
+      <div className="box v">10</div>
+      <div className="box w">10</div>
+      <div className="box x">10</div>
+      <div className="box y">10</div>
+      <div className="box b">10</div>
+    </div>
   );
 }
 
