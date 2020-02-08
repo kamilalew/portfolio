a0b85251a660fa18ebe48f661ad1b3b9025e0631
Kamila <kamila.m.lewicka@gmail.com>
2020-02-08 23:02:33 +0000

    My projects section added and initial media queries

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import html5 from './images/html5.png'
 import css from './images/css.png'
 import js from './images/js.png'
 import react from './images/react.png'
-import arrow from './images/arrow.jpg'
+import node from './images/node.png'
 
 
 function App() {
@@ -19,38 +19,50 @@ function App() {
       <div className="box z">
         <div className="aboutMe">Junior Front End Web Developer <br /><br /> London based</div>
       </div>
+      <div className="box x">
+        <div className="myLinks_wrap">
+          <a href='https://github.com/kamilalew' target='_blank' rel="noopener noreferrer"><img className="myLinks myLinks1" src={github} alt="github" /></a>
+          <a href='https://www.linkedin.com/in/kamila-m-lewicka/' target='_blank' rel="noopener noreferrer"><img className="myLinks" src={linkedIn} alt="LinkedIn" /></a>
+        </div>
+        <p className="email">kamila.m.lewicka@gmail.com</p>
+      </div>
       <div className="box j">About Me</div>
       <div className="box b">
         <div className='myPhoto'></div>
       </div>
       <div className="box o">
-        <div className="aboutMe_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi magna, fringilla vitae tempor tempus, dapibus nec nisi. Integer pellentesque ultricies ante. Ut tellus turpis, pretium nec tempus vitae, fringilla fermentum nisl. Quisque vulputate feugiat eros non sagittis. Sed viverra diam nunc, at malesuada nisl condimentum id. Nullam varius vel risus eu interdum. Suspendisse sed maximus nunc. Praesent vitae sapien ante. Phasellus at ex augue. Integer malesuada odio aliquam lectus mollis eleifend. Suspendisse potenti. Curabitur molestie lorem vel aliquam viverra. Nullam sagittis rhoncus dolor, vitae volutpat diam rhoncus non.</div>
+        <div className="aboutMe_description">Lorem ipsum dolor sit amet, consectetur adipiscvitae tempor tempus, dapibus nec nisi. Integer pellentesque ultricies ante. Ut tellus turpis, pretium nec tempus s eu interdum. Suspendisse sed maximus nunc. Praesent vitae sapien ante. Phasellus at ex augue. Integer malesuada odio aliquam lectus mollis eleifend. Suspendisse potenti. Curabitur molestie lorem vel aliquam viverra. Nullam sagittis rhoncus dolor, vitae volutpat diam rhoncus non.</div>
       </div>
       <div className="box v"></div>
-      <div className="box x">
-        <div className="myLinks_wrap">
-          <a href='https://github.com/kamilalew' target='_blank' rel="noopener noreferrer"><img className="myLinks" src={github} alt="github" /></a>
-          <a href='https://www.linkedin.com/in/kamila-m-lewicka/' target='_blank' rel="noopener noreferrer"><img className="myLinks" src={linkedIn} alt="LinkedIn" /></a>
-        </div>
-        <p className="email">kamila.m.lewicka@gmail.com</p>
-      </div>
       <div className="box c">Technologies I use</div>
       <div className="box d"><img className="technologies" src={html5} alt="html5" /></div>
       <div className="box e"><img className="technologies" src={css} alt="css" /></div>
       <div className="box f"><img className="technologies" src={js} alt="js" /></div>
       <div className="box g"><img className="technologies" src={react} alt="react" /></div>
+      <div className="box p"><img className="technologies" src={node} alt="node" /></div>
       <div className="box h">
         <div className="projects">My Projects</div>
       </div>
-      <div className="box k">
-      <div className="londonSites_image"><a href="https://london-sites.now.sh" /></div>
-      <div className="LondonSites_description">London <br/>Sites</div>
-      </div>
-      <div className="box l">10</div>
-      <div className="box m">10</div>
-      <div className="box n">10</div>
+      <div className="box k"><a href="https://london-sites.now.sh"
+        alt="london_sites" target="_blank" rel="noopener noreferrer">
+        <div className="londonSites_image"></div>
+        <div className="LondonSites_description">London <br />Sites</div>
+      </a></div>
+      <div className="box l"><a href="https://kodflix-kamilalew.herokuapp.com"
+        alt="kodflix" target="_blank" rel="noopener noreferrer">
+        <div className="kodflix_image"></div>
+        <div className="kodflix_description">Kodflix</div>
+      </a></div>
+      <div className="box m"><a href="https://cat-adoption.now.sh/"
+        alt="catAdoption" target="_blank" rel="noopener noreferrer">
+        <div className="catAdoption_image"></div>
+        <div className="catAdoption_description">Cat <br />Adoption</div>
+      </a></div>
+      <div className="box n"><a href=""
+      alt="WePair" target="_blank" rel="noopener noreferrer">
+      <div className="wePair_description">WePair <br/> <span className="comingSoon">Coming soon</span></div>
+    </a></div>
       <div className="box o">10</div>
-      <div className="box p">10</div>
       <div className="box q">10</div>
       <div className="box r">10</div>
       <div className="box s">10</div>
