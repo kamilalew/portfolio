c0c1644cf8e31e40776d56cbec8ff1c3989c806b
Kamila Lew <kamilalew@Kamilas-MBP.home>
2021-04-18 20:46:44 +0100

    Update

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -10,10 +10,12 @@ export default function AboutMe() {
                 <div className='myPhoto'></div>
             </div>
             <div className="box o">
-                <div className="aboutMe_description" align="justify">I am an extremely motivated individual, always ready to learn new things.
-                My passion is coding therefore I've decided to give up my previous career to become a professional web/app developer.
-          <br></br><br></br>Over the last year I've been broadening my coding skills studying on my own, pairing with others during the coding bootcamp and attending various coding meet ups across London.
-                    Looking for a front end position in a fast developing, future focused company.
+                <div className="aboutMe_description">I am an extremely motivated self-taught frontend web developer, always ready to learn new things.
+                <br/>
+
+                My passion is coding therefore I've decided to give up my previous career to become a professional web/app developer
+                <br /> <br />
+                I currently work for well known international company, building powerful web applications with JavaScript (ES6+), UIkit, Vue.js and Nuxt.js
           </div>
             </div>
             <div className="box v"></div>
--- src/Technologies/Technologies.js
+++ src/Technologies/Technologies.js
@@ -4,7 +4,7 @@ import html5 from '../images/html5.png'
 import css from '../images/css.png'
 import js from '../images/js.png'
 import react from '../images/react.png'
-import node from '../images/node.png'
+import vue from '../images/vue.png'
 
 
 export default function Technologies() {
@@ -14,8 +14,8 @@ export default function Technologies() {
             <div className="box d"><img className="technologies" src={html5} alt="html5" /></div>
             <div className="box e"><img className="technologies" src={css} alt="css" /></div>
             <div className="box f"><img className="technologies" src={js} alt="js" /></div>
-            <div className="box g"><img className="technologies" src={react} alt="react" /></div>
-            <div className="box p"><img className="technologies" src={node} alt="node" /></div>
+            <div className="box g"><img className="technologies" src={vue} alt="vue" /></div>
+            <div className="box p"><img className="technologies" src={react} alt="react" /></div>
         </React.Fragment>
     );
 }
